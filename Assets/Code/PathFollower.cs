4a8553c71bc66f256374352f55193048249e6926
GeorgeFritz8 <30572@ma-web.nl>
2021-11-05 15:01:38 +0100

    background

--- Assets/Code/PathFollower.cs
+++ Assets/Code/PathFollower.cs
@@ -47,7 +47,6 @@ public class PathFollower : MonoBehaviour
     {
         if (other.gameObject.CompareTag("AOE"))
         {
-            print("egg");
             AOEDamage = true;
         }
     }
